191aee5f420b46ae24cc1e225866dcb8be5d7880
GavBaros <30755017+GavBaros@users.noreply.github.com>
2019-02-04 11:20:25 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import Calculator from "./components/Calculator";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Calculator />, document.getElementById("root"));
